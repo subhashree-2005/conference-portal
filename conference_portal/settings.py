f00f3f7bce3639cf7252d42cf151debff5da0bc3
Subhashree Aich <subhashreeaich55@gmail.com>
2026-06-29 15:44:47 +0530

    Added email debugging in settings.py

--- conference_portal/settings.py
+++ conference_portal/settings.py
@@ -138,8 +138,9 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "subhashreeaich55@gmail.com"
+
+EMAIL_HOST_PASSWORD = "easw tveu xbct ddxf"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # -----------------------------
